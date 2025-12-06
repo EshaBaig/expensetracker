4f86a31edc9d48ff638f31ee758dbc8d5f16a6f1
EshaBaig <222571199+eshabaig724@users.noreply.github.com>
2025-12-06 16:52:40 +0500

    starter created

--- ios/starter/AppDelegate.swift
+++ ios/starter/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "expensetracker",
+      withModuleName: "starter",
       in: window,
       launchOptions: launchOptions
     )
